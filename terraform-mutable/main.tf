9bd91a2a759abe6c4752ad1966db8f6f0a28846c
ajitnitr <92538695+ajitnitr@users.noreply.github.com>
2022-01-27 11:50:47 +0530

    Update main.tf

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -1,5 +1,5 @@
 module "app-module" {
-  source           = "git::https://github.com/raghudevopsb61/terraform-mutable-app-module.git"
+  source           = "git::https://github.com/ajitnitr/terraform-mutable-app-module.git"
   INSTANCE_COUNT   = var.INSTANCE_COUNT
   INSTANCE_TYPE    = var.INSTANCE_TYPE
   COMPONENT        = var.COMPONENT
